348a6790f0f4b0c9700c66a791611cc502b54c17
Gary Justin <gary.justin@forrent.com>
2016-08-25 18:28:05 -0400

    remove tmx use, update deps

--- tasks/locale-html.js
+++ tasks/locale-html.js
@@ -8,50 +8,66 @@
 
 require('date-util');
 
-var minify = require('html-minifier').minify,
-    parser = require('xml2json'),
-    _ = require('underscore'),
-    i18nReferenceWritten = false;
+var minify = require('html-minifier').minify;
+var _ = require('underscore');
 
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    grunt.registerMultiTask('localeHtml', 'Translate HTML Template', function() {
+    grunt.registerMultiTask('localeHtml', 'Translate HTML Template', function () {
         var defaults = {
             flatten: false,
-            minify: false
+            minify: false,
+            variablesFile: process.cwd() + '/variables.json',
+            locales: [
+                {
+                    id: 'en',
+                    localeCue: 'English',
+                    localeCueCode: 'en',
+                    isPrimary: true
+                }, {
+                    id: 'es',
+                    localeCue: 'En Español',
+                    localeCueCode: 'es'
+                }
+            ]
         };
         var options = _.extend({}, defaults, this.options()),
 
             /**
              * Build the object that we will output for reference and use to generate the HTML
              * from Underscore templates.
-             * @param <Object> tmxObj - The tmx file translated into a JavaScript object.
-             * @param <Array> locales - An array of all of the locales in the .tmx file.
+             * @param <Object> dictionary - Translation definitions.
              * @return <Object> i18nReference - The reference object for the internalization translation.
              */
-            buildi18nReferenceObj = function(tmxObj, locales) {
+            buildi18nReferenceObj = function (dictionary) {
                 var i18nReference = {};
 
-                locales.forEach(function(locale) {
-                    i18nReference[locale] = {};
+                options.locales.forEach(function (locale) {
+                    i18nReference[locale.id] = {};
 
-                    i18nReference[locale].localeId = locale;
-                    i18nReference[locale].localeCode = locale === 'en' ? '' : locale;
+                    i18nReference[locale.id].localeId = locale.id;
+                    i18nReference[locale.id].localeCode = locale.isPrimary ? '' : locale.id;
 
                     // TODO - These will not work when multiple translations
-                    i18nReference[locale].localeCueCode = locale === 'en' ? 'es' : 'en';
-                    i18nReference[locale].localeCue = locale === 'en' ? 'En Español' : 'English';
-                    i18nReference[locale].currentYear = new Date().format('yyyy');
+                    // There's no way to make this work for multiple translations without breaking or modifying the front end.
+                    i18nReference[locale.id].localeCueCode = locale.id === 'en' ? 'es' : 'en';
+                    i18nReference[locale.id].localeCue = locale.id === 'en' ? 'En Español' : 'English';
+                    i18nReference[locale.id].currentYear = new Date().format('yyyy');
                 });
 
-                tmxObj.tmx.body.tu.forEach(function(tu) {
-                    if('tuid' in tu) {
-                        var variable = convertVarFromWords(tu.tuid);
-                        i18nReference['en'][variable] = tu.tuid;
+                Object.keys(dictionary).forEach(function (key) {
+                    if (key) {
+                        var variable = convertVarFromWords(key);
 
-                        if('tuv' in tu && 'xml:lang' in tu.tuv && 'seg' in tu.tuv) {
-                            i18nReference[tu.tuv['xml:lang']][variable] = tu.tuv.seg;
+                        if (dictionary[key]) {
+                            options.locales.forEach(function (locale) {
+                                if (!locale.isPrimary) {
+                                    i18nReference[locale.id][variable] = dictionary[key][locale.id];
+                                } else {
+                                    i18nReference[locale.id][variable] = key;
+                                }
+                            });
                         }
                     }
                 });
@@ -60,39 +76,19 @@ module.exports = function(grunt) {
             },
 
             /**
-             * Convert a .tmx file to a JavaScript object
-             * @param <String> tmxFile
-             * @return <Object> Null if this process fails.
-             */
-            convertTMXtoJSObject = function(tmxFile) {
-                try {
-                    return parser.toJson(grunt.file.read(options.tmx), {
-                        object: true,
-                        reversible: false,
-                        coerce: true,
-                        sanitize: false,
-                        trim: true,
-                        arrayNotation: false
-                    });
-                } catch(e) {
-                    return null;
-                }
-            },
-
-            /**
              * Convert a string of words to a camelcase variable name.
              * @param <String> words
              * @return <String>
              */
-            convertVarFromWords = function(words) {
+            convertVarFromWords = function (words) {
                 words = ucwords(words).replace(/[^A-Za-z0-9]/g, '');
-                if(words.length > 30) {
-                    words = words.substr(0, 30) +'_';
+                if (words.length > 30) {
+                    words = words.substr(0, 30) + '_';
                 }
 
                 // Variable names can't start with #'s. Add digit_ string to fix this issue.
-                if(/^\d$/.test(words.substr(0,1))) {
-                    words = '_'+ words;
+                if (/^\d$/.test(words.substr(0, 1))) {
+                    words = '_' + words;
                 }
 
                 return words.substr(0, 1).toLowerCase() + words.substr(1);
@@ -105,12 +101,12 @@ module.exports = function(grunt) {
              * @param <String> folder
              * @param <String> file
              */
-            generateDestinationFileName = function(srcString, locale, folder, file) {
-                if(options.flatten){
+            generateDestinationFileName = function (srcString, locale, folder, file) {
+                if (options.flatten) {
                     var parts = file.split('/');
-                    return folder +'/'+ locale +'/' + parts[parts.length -1];
+                    return folder + '/' + locale + '/' + parts[parts.length - 1];
                 }
-                return folder +'/'+ locale +'/' +
+                return folder + '/' + locale + '/' +
                     file.replace(file.substring(0, srcString.indexOf('/*') + 1), '');
             },
 
@@ -122,26 +118,26 @@ module.exports = function(grunt) {
              * @param <Array> locales - Array of all of the locales used in the .tmx file.
              * @param <Object> i18nReference - The reference object that will be used with Underscore to translate the templates.
              */
-            generateHTMLFiles = function(files, destFolder, srcString, locales, i18nReference) {
-                files.forEach(function(file) {
+            generateHTMLFiles = function (files, destFolder, srcString, locales, i18nReference) {
+                files.forEach(function (file) {
                     var tmpl = null;
                     try {
                         tmpl = _.template(grunt.file.read(file));
-                    } catch (e) {}
+                    } catch (e) { }
 
-                    if(tmpl === null) {
-                        grunt.log.warn('"'+ file +'" had an Underscore variable that was not found in "'+ options.i18n +'".');
+                    if (tmpl === null) {
+                        grunt.log.warn('"' + file + '" had an Underscore variable that was not found in "' + options.i18n + '".');
                         return;
                     }
 
-                    locales.forEach(function(locale) {
-                        var htmlContent = tmpl(i18nReference[locale]);
+                    locales.forEach(function (locale) {
+                        var htmlContent = tmpl(i18nReference[locale.id]);
 
-                        if(options.minify) {
+                        if (options.minify) {
                             htmlContent = minifyHTML(htmlContent);
                         }
 
-                        writeHTMLFile(generateDestinationFileName(srcString, locale, destFolder, file), htmlContent);
+                        writeHTMLFile(generateDestinationFileName(srcString, locale.id, destFolder, file), htmlContent);
                     });
                 });
             },
@@ -151,7 +147,7 @@ module.exports = function(grunt) {
              * @param <String> htmlContent
              * @return <String>
              */
-            minifyHTML = function(htmlContent) {
+            minifyHTML = function (htmlContent) {
                 try {
                     return minify(htmlContent, {
                         removeComments: true,
@@ -163,27 +159,11 @@ module.exports = function(grunt) {
             },
 
             /**
-             * Determine all of the locales that are used in the .tmx file.
-             * @param <Object> tmxObj - JavaScript object parsed from the .tmx file.
-             * @return <Array> locales - Array of two character strings
-             */
-            parseLocalesFromTMX = function(tmxObj) {
-                var locales = ['en'];
-                tmxObj.tmx.body.tu.forEach(function(tu) {
-                    if('tuid' in tu && 'tuv' in tu && 'xml:lang' in tu.tuv && locales.indexOf(tu.tuv['xml:lang']) === -1) {
-                        locales.push(tu.tuv['xml:lang']);
-                    }
-                });
-
-                return locales;
-            },
-
-            /**
              * Capitalize the first letter of every word in string.
              * @param <String> str
              * @return <String>
              */
-            ucwords = function(str) {
+            ucwords = function (str) {
                 return (str + '').replace(/^([a-z])|\s+([a-z])/g, function ($1) {
                     return $1.toUpperCase();
                 });
@@ -193,16 +173,16 @@ module.exports = function(grunt) {
              * Make sure that provided object has required properties and values are valid.
              * @param <Object> file - HTML template file (Underscore.js template)
              */
-            validateFile = function(file) {
+            validateFile = function (file) {
                 // Each file should have the following properties: src, dest and locale
-                if(!('src' in file) || !('dest' in file)) {
+                if (!('src' in file) || !('dest' in file)) {
                     return grunt.log.warn('Configuration is not properly set up.  Each file must have src, dest and locale.');
                 }
 
-                file.src.forEach(function(srcFile) {
+                file.src.forEach(function (srcFile) {
                     // Make sure that the source HTML file exists.
-                    if(!grunt.file.exists(srcFile)) {
-                        return grunt.log.warn('The source file "'+ file.src +'" was not found.');
+                    if (!grunt.file.exists(srcFile)) {
+                        return grunt.log.warn('The source file "' + file.src + '" was not found.');
                     }
                 });
             },
@@ -211,39 +191,14 @@ module.exports = function(grunt) {
              * Make sure that Gruntfile has provided all of the required options and that
              * the options have valid entries.
              */
-            validateOptions = function() {
-                // tmx will point to the .tmx file that we will use to determine locale values.
-                if(!('tmx' in options)) {
-                    return grunt.log.warn('Configuration is not properly set up.  tmx file not provided.');
-                }
-
-                // .tmx file not found
-                if(!grunt.file.exists(options.tmx)) {
-                    return grunt.log.warn('tmx file was not found!  Please double check that "'+ options.tmx +'" exists.');
-                }
-
+            validateOptions = function () {
                 /**
                  * i18n will provide the path and file name for the json interpretation of the tmx file.
                  * This will be a reference file for variable names.
                  */
-                if(!('i18n' in options)) {
+                if (!('i18n' in options)) {
                     return grunt.log.warn('Configuration is not properly set up.  i18n option not provided.');
                 }
-            },
-
-            /**
-             * Generate a facebook crawler file provided with the appropriate html input file.
-             * @param <String> htmlFile
-             * @param <String> crawlerFile
-             */
-            writeFacebookCrawlerFile = function(htmlFile, phpFile, crawlerFile) {
-                var htmlContents = grunt.file.read(htmlFile),
-                    phpContents = grunt.file.read(phpFile),
-                    headIdx = htmlContents.indexOf('</head>');
-
-                grunt.file.write(crawlerFile, htmlContents.substr(0, headIdx) +
-                    phpContents + htmlContents.substr(headIdx));
-                grunt.log.writeln('Successfully generated translated PHP file "'+ crawlerFile +'".');
             },
 
             /**
@@ -251,25 +206,24 @@ module.exports = function(grunt) {
              * @param <String> htmlFile
              * @param <String> htmlContents
              */
-            writeHTMLFile = function(htmlFile, htmlContents) {
+            writeHTMLFile = function (htmlFile, htmlContents) {
                 grunt.file.write(htmlFile, htmlContents);
-                grunt.log.writeln('Successfully generated translated HTML file "'+ htmlFile +'".');
+                grunt.log.writeln('Successfully generated translated HTML file "' + htmlFile + '".');
             };
 
 
         validateOptions();
 
-        var tmxObj = convertTMXtoJSObject(options.tmx),
-            locales = parseLocalesFromTMX(tmxObj),
-            i18nReference = buildi18nReferenceObj(tmxObj, locales);
+        var dictionary = grunt.file.readJSON(options.i18n);
+        var i18nReference = buildi18nReferenceObj(dictionary);
 
         this.files.forEach(function(file) {
             validateFile(file);
-            generateHTMLFiles(file.src, file.dest, file.orig.src[0], locales, i18nReference);
+            generateHTMLFiles(file.src, file.dest, file.orig.src[0], options.locales, i18nReference);
         });
 
         // Write the i18n reference file.
-        grunt.file.write(options.i18n, JSON.stringify(i18nReference, null, 4));
-        grunt.log.writeln('Successfully generated internalization reference file: "'+ options.i18n +'".');
+        grunt.file.write(options.variablesFile, JSON.stringify(i18nReference, null, 4));
+        grunt.log.writeln('Successfully generated internalization reference file: "' + options.variablesFile + '".');
     });
 };
